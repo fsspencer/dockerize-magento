035dd4a2e3025b846346f43d334420dcdbe7689b
Francis S. Spencer <fsspencer@Franciss-MacBook-Pro.local>
2017-12-21 21:19:20 -0300

    Config files added

---
+++ config/m2/ce/env.php
@@ -0,0 +1,60 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => '[BACKEND_FRONTNAME]',
+  ),
+  'crypt' => 
+  array (
+    'key' => '[CRYPT]', // sample: 6954a1ff91e2bb80bba35ebbaf35bcc9
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => '[DB_HOST]',
+        'dbname' => '[DB_NAME]',
+        'username' => '[DB_USER]',
+        'password' => '[DB_PASSWORD]',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+    'compiled_config' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => '[DATE]', // sample: Fri, 03 Mar 2017 13:26:52 +0000
+  ),
+);
---
+++ config/m2/ee/env.php
@@ -0,0 +1,69 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => '[BACKEND_FRONTNAME]',
+  ),
+  'db' => 
+  array (
+    'connection' => 
+    array (
+      'indexer' => 
+      array (
+        'host' => '[DB_HOST]',
+        'dbname' => '[DB_NAME]',
+        'username' => '[DB_USER]',
+        'password' => '[DB_PASSWORD]',
+        'active' => '1',
+        'persistent' => NULL,
+      ),
+      'default' => 
+      array (
+        'host' => '[DB_HOST]',
+        'dbname' => '[DB_NAME]',
+        'username' => '[DB_USER]',
+        'password' => '[DB_PASSWORD]',
+        'active' => '1',
+      ),
+    ),
+    'table_prefix' => '',
+  ),
+  'crypt' => 
+  array (
+    'key' => '[CRYPT]', // sample: 6954a1ff91e2bb80bba35ebbaf35bcc9
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'target_rule' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => '[DATE]', // sample: Fri, 03 Mar 2017 13:26:52 +0000
+  ),
+);
